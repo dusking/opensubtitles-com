a50a32667177a34516690cc89d596fb47de01d7e
dusking@gmail.com <dusking@gmail.com>
2023-09-22 09:53:54 +0300

    bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def parse_requirements(filename) -> List[str]:
 install_reqs = parse_requirements("requirements.txt")
 requirements = [str(ir) for ir in install_reqs]
 
-version = "0.0.2"
+version = "0.0.3"
 
 setup_kwargs = dict(
     name="opensubtitlescom",
