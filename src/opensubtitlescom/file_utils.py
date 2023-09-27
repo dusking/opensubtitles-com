24630ee840486cbd9c3f94feab36d3a4d4b07d33
Roy Zmora <roy.zmora@gmail.com>
2023-09-27 23:23:34 +0300

    feature/get_hash add get_hash to file_utils.py

--- src/opensubtitlescom/file_utils.py
+++ src/opensubtitlescom/file_utils.py
@@ -10,8 +10,12 @@ Institute of Technology.
 For full details, please see the LICENSE file located in the root
 directory of this project.
 """
+import struct
+import hashlib
 
 from pathlib import Path
+
+from .exceptions import OpenSubtitlesFileException
 
 
 class FileUtils:
@@ -51,3 +55,35 @@ class FileUtils:
             PermissionError if the filesystem permissions deny the operation.
         """
         return self.path.exists()
+
+    def get_hash(self):
+        """
+        Return the hash code of a file.
+​
+        Original from: https://trac.opensubtitles.org/projects/opensubtitles/wiki/HashSourceCodes.
+        """
+        if not self.exists():
+            raise OpenSubtitlesFileException(f"File not exists: {self.path}")
+        size = self.path.stat().st_size
+        longlongformat = "q"  # long long
+        bytesize = struct.calcsize(longlongformat)
+
+        if int(size) < 65536 * 2:
+            raise OpenSubtitlesFileException("SizeError")
+
+        with open(self.path, "rb") as file_obj:
+            hash = size
+            for _ in range(65536 // bytesize):
+                buffer = file_obj.read(bytesize)
+                (l_value,) = struct.unpack(longlongformat, buffer)
+                hash += l_value
+                hash = hash & 0xFFFFFFFFFFFFFFFF  # to remain as 64bit number
+
+        file_obj.seek(max(0, int(size) - 65536), 0)
+        for _ in range(65536 // bytesize):
+            buffer = file_obj.read(bytesize)
+            (l_value,) = struct.unpack(longlongformat, buffer)
+            hash += l_value
+            hash = hash & 0xFFFFFFFFFFFFFFFF
+
+        return str("%016x" % hash)
